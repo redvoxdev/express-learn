94cbadfab0b17e7546a3118b9e05f2ed9f489e93
redvoxdev <redvoxdev@gmail.com>
2022-09-09 23:45:24 +0600

    fix: pseudoauth if no token

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 
 app.get('/api/v1/data', (req, res) => {
-  const authHeader = req.headers['cookie'];
-  const token = authHeader.split('=')[1]
-  if (token && token === process.env.token) {
-    res.send('Hello World!')
-  } else {
-    res.status(403).send("Token invalid")
+  const authHeader = req.headers['cookie']
+  if (authHeader && authHeader.split('=').length > 1) {
+    const token = authHeader.split('=')[1]
+    if (token === process.env.token) {
+      res.send('Hello World!')
+    } else {
+      res.status(403).send("Token invalid")
+    }
   }
 })
 
