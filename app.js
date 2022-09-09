cb01bd4a948d713868d382fb31a808e9114fe267
redvoxdev <redvoxdev@gmail.com>
2022-09-09 23:33:47 +0600

    feat: add pseudoauth

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/api/v1/data', (req, res) => {
+  const authHeader = req.headers['cookie'];
+  const token = authHeader.split('=')[1]
+  if (token && token === process.env.token) {
+    res.send('Hello World!')
+  } else {
+    res.status(403).send("Token invalid")
+  }
 })
 
 module.exports = app
